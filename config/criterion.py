9a9aea83ee017829eed6ecaf3b226dc10f9d5f42
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-01-13 12:07:13 +0900

    Fixed bugs of deepsurv.

--- config/criterion.py
+++ config/criterion.py
@@ -40,7 +40,7 @@ class Regularization(object):
 class NegativeLogLikelihood(nn.Module):
     def __init__(self, device):
         super(NegativeLogLikelihood, self).__init__()
-        self.L2_reg = 0.08
+        self.L2_reg = 0.05
         self.reg = Regularization(order=2, weight_decay=self.L2_reg)
         self.device = device
 
@@ -48,14 +48,14 @@ class NegativeLogLikelihood(nn.Module):
         mask = torch.ones(y.shape[0], y.shape[0]).to(self.device)
         mask[(y.T - y) > 0] = 0
         loss_1 = torch.exp(risk_pred) * mask
-        loss_2 = torch.sum(loss_1, dim=0) / torch.sum(mask, dim=0)
-        loss_3 = torch.log(loss_2).reshape(-1, 1)
+        loss_1 = torch.sum(loss_1, dim=0) / torch.sum(mask, dim=0)
+        loss_1 = torch.log(loss_1).reshape(-1, 1)
         num_occurs = torch.sum(e)
         if num_occurs.item() == 0.0:
             # To avoid dividing with zero, set small value as loss
             loss = torch.tensor([1e-7], requires_grad=True)
         else:
-            neg_log_loss = -torch.sum((risk_pred-loss_3) * e) / num_occurs
+            neg_log_loss = -torch.sum((risk_pred-loss_1) * e) / num_occurs
             l2_loss = self.reg(model)
             loss = neg_log_loss + l2_loss
         return loss
