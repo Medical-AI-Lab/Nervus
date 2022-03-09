848c63486a196bb5bdd35b58fd8d5375f473c4ab
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-03-09 18:28:54 +0900

    Refactor: make def parce_csv() class and rename output_ label_.

--- lib/align_env.py
+++ lib/align_env.py
@@ -26,85 +26,106 @@ class NervusEnv:
     csv_summary: str = 'summary.csv'
 
 
-def parse_csv(csv_path, task):
-    csv_dict = {} 
-    prefix_id = 'id'  # 'id'
-    prefix_input = 'input'
-    prefix_output = 'output'
-    prefix_label = 'label'
-    prefix_period = 'periods'
+class SplitProvider:
+    def __init__(self, split_path, task):
+        super().__init__()
 
-    csv_dict['filepath_column'] = 'filepath'
-    csv_dict['split_column'] = 'split'
+        self.split_path = split_path
+        self.task = task
 
-    # Exclude
-    df_source = pd.read_csv(csv_path)
-    df_source = df_source[df_source[csv_dict['split_column']] != 'exclude']
+        self.prefix_id = 'id'
+        self.prefix_input = 'input'
+        self.prefix_label = 'label'
+        self.prefix_internal_label = 'internal'
+        self.prefix_period = 'periods'
 
-    # Make dict for labeling
-    # csv_dict['output_class_label'] =
-    # {'output_1':{'A':0, 'B':1}, 'output_2':{'C':0, 'D':1, 'E':2}, ...}   classification
-    # {'output_1':{}, 'output_2':{}, ...}                                  regression 
-    # {'output_1':{'A':0, 'B':1}}                                          deepsurv
-    csv_dict['output_list'] = [column_name for column_name in list(df_source.columns) if column_name.startswith(prefix_output)]
-    output_class_label_dict = {}
-    for output_name in csv_dict['output_list']:
-        class_list = df_source[output_name].value_counts().index.tolist()
-        output_class_label_dict[output_name] = {}
-        if (task == 'classification') or (task == 'deepsurv'):
-             for i in range(len(class_list)):
-                output_class_label_dict[output_name][class_list[i]] = i
+        self.filepath_column = 'filepath'
+        self.split_column = 'split'
+
+        _df_source = pd.read_csv(self.split_path)
+        _df_source = _df_source[_df_source[self.split_column] != 'exclude'].copy()
+        _df_source_labeled, self.class_name_in_label = self._make_labelling(_df_source, self.task)   # Labeling
+
+        # cast
+        self.df_source = self._cast_csv(_df_source_labeled, task)
+        self.label_num_classes = self._define_label_num_classes(self.df_source, self.task)    # After labeling, column names are fixed.
+
+        self.label_list = list(self.df_source.columns[self.df_source.columns.str.startswith(self.prefix_label)])
+        self.internal_label_list = list(self.df_source.columns[self.df_source.columns.str.startswith(self.prefix_internal_label)])
+        self.input_list = list(self.df_source.columns[self.df_source.columns.str.startswith(self.prefix_input)])
+        self.num_inputs = len(self.input_list)
+
+        if self.task == 'deepsurv':
+            self.period_column = list(self.df_source.columns[self.df_source.columns.str.startswith(self.prefix_period)])[0]  # should be one
         else:
-            output_class_label_dict[output_name] = {}   # No need of labeling
-    csv_dict['output_class_label'] = output_class_label_dict
+            self.period_column = None
+
 
     # Labeling
-    for output_name, class_label_dict in output_class_label_dict.items():
-        if (task == 'classification') or (task == 'deepsurv'):
-            for class_name, label in class_label_dict.items():
-                df_source.loc[df_source[output_name]==class_name, (prefix_label + '_' + output_name)] = label
-        else:
-            df_source[(prefix_label + '_' + output_name)] = df_source[output_name]   # Just copy. Needed because labeL_XXX will be cast later.
+    def _make_labelling(self, df_source, task):
+        # Make dict for labeling
+        # _class_name_in_label =
+        # {'label_1':{'A':0, 'B':1}, 'label_2':{'C':0, 'D':1, 'E':2}, ...}   classification
+        # {'label_1':{}, 'label_2':{}, ...}                                  regression
+        # {'label_1':{'A':0, 'B':1}}                                         deepsurv
+        _df_tmp = df_source.copy()
+        _label_list = list(_df_tmp.columns[_df_tmp.columns.str.startswith(self.prefix_label)])
+        _class_name_in_label = {}
+        for label_name in _label_list:
+            class_list = _df_tmp[label_name].value_counts().index.tolist()    # {'A', 'B', ... } decending order
+            _class_name_in_label[label_name] = {}
+            if (task == 'classification') or (task == 'deepsurv'):
+                 for i in range(len(class_list)):
+                    _class_name_in_label[label_name][class_list[i]] = i
+            else:
+                _class_name_in_label[label_name] = {}                         # No need of labeling
 
-    # After labeling, column names are fixed.
-    column_names = list(df_source.columns)
+        # Labeling
+        for label_name, class_name_in_label in _class_name_in_label.items():
+            _internal_label = self.prefix_internal_label + '_' + label_name      # label_XXX -> internal_label_XXX
+            if (task == 'classification') or (task == 'deepsurv'):
+                for class_name, ground_truth in class_name_in_label.items():
+                    _df_tmp.loc[_df_tmp[label_name]==class_name, _internal_label] = ground_truth
+            else:
+                _df_tmp[_internal_label] = _df_tmp[label_name]    # Just copy. Needed because internal_label_XXX will be cast later.
 
-    # Define the number of outputs of mode
-    # csv_dict['label_num_classes'] =
-    # {label_output_1: 2, label_output_2: 3, ...}   classification
-    # {label_output_1: 1, label_output_2: 1, ...}   regression,  should be 1
-    # {label_output_1: 1}                           deepsurv,    should be 1
-    csv_dict['label_list'] = [column_name for column_name in column_names if column_name.startswith(prefix_label)]
-    csv_dict['label_num_classes'] = {}
-    for label_name in csv_dict['label_list']:
-        if task == 'classification':
-            csv_dict['label_num_classes'][label_name] = df_source[label_name].nunique()
-        else:
-            # When regression or deepsurv
-            csv_dict['label_num_classes'][label_name] = 1
+        return _df_tmp, _class_name_in_label
 
-    csv_dict['id_column'] = [column_name for column_name in column_names if column_name.startswith(prefix_id)][0]   # should be one
-    csv_dict['input_list'] = [column_name for column_name in column_names if column_name.startswith(prefix_input)]
-    csv_dict['num_inputs'] = len(csv_dict['input_list'])
 
-    if task == 'deepsurv':
-        csv_dict['period_column'] = [column_name for column_name in column_names if column_name.startswith(prefix_period)][0]   # should be one
-    else:
-        csv_dict['period_column'] = None
+    def _define_label_num_classes(self, df_source, task):
+        # _label_num_classes
+        # {label_output_1: 2, label_output_2: 3, ...}   classification
+        # {label_output_1: 1, label_output_2: 1, ...}   regression,  should be 1
+        # {label_output_1: 1}                           deepsurv,    should be 1
+        _label_num_classes = {}
+        _label_list = list(df_source.columns[df_source.columns.str.startswith(self.prefix_label)])
+        for label_name in _label_list:
+            if task == 'classification':
+                _label_num_classes[label_name] = df_source[label_name].nunique()
+            else:
+                _label_num_classes[label_name] = 1
+
+        return _label_num_classes
+
 
     # Cast
-    # label_* : int
-    # input_* : float
-    cast_input_dict = {input: float for input in csv_dict['input_list']}
-    if task == 'classification':
-        cast_label_dict = {label: int for label in csv_dict['label_list']}
-    else:
-        # When regression or deepsurv
-        cast_label_dict = {label: float for label in csv_dict['label_list']}
-    df_source = df_source.astype(cast_input_dict)
-    df_source = df_source.astype(cast_label_dict)
+    def _cast_csv(self, df_source, task):
+        # label_* : int
+        # input_* : float
+        _df_tmp = df_source.copy()
+        _input_list = list(df_source.columns[df_source.columns.str.startswith(self.prefix_input)])
+        _internal_label_list = list(df_source.columns[df_source.columns.str.startswith(self.prefix_internal_label)])
 
-    csv_dict['source'] = df_source
-    return csv_dict
+        _cast_input_dict = {input: float for input in _input_list}
+        
+        if task == 'classification':
+            _cast_internal_label_dict = {label: int for label in _internal_label_list}
+        else:
+            # When regression or deepsurv
+            _cast_internal_label_dict = {label: float for label in _internal_label_list}
 
-# ----- EOF -----
+        _df_tmp = _df_tmp.astype(_cast_input_dict)
+        _df_tmp = _df_tmp.astype(_cast_internal_label_dict)
+
+        return _df_tmp
+
