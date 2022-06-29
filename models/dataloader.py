a6ec7bd9764e218fe934a92745ac053b5936b05d
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-06-29 17:23:24 +0900

    Modified and debugging.

--- models/dataloader.py
+++ models/dataloader.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from abc import ABC, abstractmethod
-import os
 import sys
+from pathlib import Path
 import numpy as np
 import pandas as pd
 
@@ -11,186 +10,179 @@ import torch
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
-from torch.utils.data.sampler import *
+from torch.utils.data.sampler import WeightedRandomSampler
 from PIL import Image
 from sklearn.preprocessing import MinMaxScaler
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from lib.util import *
+sys.path.append((Path().resolve() / '../').name)
+from logger.logger import NervusLogger
 
 
-class NervusDataSet(Dataset, ABC):
-    def __init__(self, args, split_provider, image_dir, split_list, multi_label=False):
+logger = NervusLogger.get_logger('dataloder')
+
+
+class NervusDataSet(Dataset):
+    def __init__(self, args, split_provider, split):
         super().__init__()
 
         self.args = args
         self.split_provider = split_provider
-        self.image_dir = image_dir
-        self.split_list = split_list   # ['train'], ['val'], ['val', 'test'], ['train', 'val', 'test']
+        self.split = split
+
+        self.raw_label_list = self.split_provider.raw_label_list
+        self.internal_label_list = self.split_provider.internal_label_list
+        self.input_list = self.split_provider.input_list
 
         self.df_source = self.split_provider.df_source
-        self.id_column = self.split_provider.id_column
-        self.institution_column = self.split_provider.institution_column
-        self.examid_column = self.split_provider.examid_column
+        self.df_split = self.split_provider.df_source[self.df_source['split'] == self.split].copy()
 
-        if multi_label:
-            self.raw_label_list = self.split_provider.raw_label_list
-            self.internal_label_list =  self.split_provider.internal_label_list
-        else:
-            self.raw_label_name = self.split_provider.raw_label_list[0]             # should be one because single-output
-            self.internal_label_name = self.split_provider.internal_label_list[0]   # should be one because single-output
-
-        self.input_list = self.split_provider.input_list
-        self.filepath_column = self.split_provider.filepath_column
-        self.split_column = self.split_provider.split_column
-        self.df_split = get_column_value(self.df_source, self.split_column, self.split_list)
-
-        # Nomalize input variables
-        if not(self.args['mlp'] is None):
-            self.input_list_normed = ['normed_' + input for input in self.input_list]
+        # Nomalize inputs
+        if (self.args.mlp is not None):
+            self.df_train = self.df_source[self.df_source['split'] == 'train'].copy()  # should be normalized with min and max of training data
             self.scaler = MinMaxScaler()
-            self.df_train = get_column_value(self.df_source, self.split_column, ['train'])   # should be normalized with min and max of training data
-            _ = self.scaler.fit(self.df_train[self.input_list])                              # fit only
-
-            inputs_normed = self.scaler.transform(self.df_split[self.input_list])
-            df_inputs_normed = pd.DataFrame(inputs_normed, columns=self.input_list_normed)
-            self.df_split = pd.concat([self.df_split, df_inputs_normed], axis=1)
+            _ = self.scaler.fit(self.df_train[self.input_list])
+            self.normed_input_list = ['normed_' + input for input in self.input_list]
+            _normed_inputs = self.scaler.transform(self.df_split[self.input_list])
+            df_normed_inputs = pd.DataFrame(_normed_inputs, columns=self.normed_input_list)
+            self.df_split = pd.concat([self.df_split, df_normed_inputs], axis=1)
 
         # Preprocess for image
-        if not(self.args['cnn'] is None):
+        if (self.args.cnn is not None):
             self.transform = self._make_transforms()
             self.augmentation = self._make_augmentations()
 
-        # index of each column
-        self.index_dict = {column_name: self.df_split.columns.get_loc(column_name) for column_name in self.df_split.columns}
-
+        self.index_dict = {col_name: self.df_split.columns.get_loc(col_name) for col_name in self.df_split.columns}  # should be after nomalization inputs
 
     def _make_transforms(self):
         _transforms = []
+        _transforms.append(transforms.ToTensor())
 
-        # MUST: Always convert to Tensor
-        _transforms.append(transforms.ToTensor())   # PIL -> Tensor
-
-        if self.args['normalize_image'] == 'yes':
-            # Normalize accepts Tensor only
-            if self.args['input_channel'] == 1:
+        if self.args.normalize_image == 'yes':
+            # transforms.Normalize accepts Tensor only.
+            if self.args.input_channel == 1:
                 _transforms.append(transforms.Normalize(mean=(0.5, ), std=(0.5, )))
-
-            elif self.args['input_channel'] == 3:
+            elif self.args.input_channel == 3:
                 _transforms.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
-
             else:
-                logger.error(f"Invalid input channel: {self.args['input_channel']}.")
+                logger.error(f"Invalid input channel: {self.args.input_channel}.")
+        else:
+            pass
 
         _transforms = transforms.Compose(_transforms)
         return _transforms
 
-
     def _make_augmentations(self):
         _augmentation = []
 
-        if self.args['preprocess'] == 'yes':
-            if self.args['random_horizontal_flip'] == 'yes':
-                _augmentation.append(transforms.RandomHorizontalFlip())
-
-            if self.args['random_rotation'] == 'yes':
-                _augmentation.append(transforms.RandomRotation((-10, 10)))
-
-            if self.args['color_jitter'] == 'yes':
-                # If img is PIL Image, mode “1”, “I”, “F” and modes with transparency (alpha channel) are not supported.
-                # When open Grayscle with "RGB" mode
-                _augmentation.append(transforms.ColorJitter())
+        if self.args.augmentation == 'randaug':
+            _augmentation.append(transforms.RandAugment())
+        elif self.args.augmentation == 'trivialaugwide':
+            _augmentation.append(transforms.TrivialAugmentWide())
+        elif self.args.augmentation == 'augmix':
+            _augmentation.append(transforms.AugMix())   # ! Cannot find in transforms
+        elif (self.args.augmentation == 'no'):
+            pass
+        else:
+            logger.error(f"Invalid augmentation: {self.args.augmentation}.")
 
         _augmentation = transforms.Compose(_augmentation)
+
         return _augmentation
 
+    def _normalized_value_to_single_tensor_if_mlp(self, idx):
+        normed_inputs_value_normed = ""
+
+        if (self.args.mlp is None):
+            return normed_inputs_value_normed
+
+        index_normed_input_list = [self.index_dict[normed_input] for normed_input in self.normed_input_list]
+        s_normed_inputs_value = self.df_split.iloc[idx,  index_normed_input_list]
+        normed_inputs_value_normed = np.array(s_normed_inputs_value, dtype=np.float64)
+        normed_inputs_value_normed = torch.from_numpy(normed_inputs_value_normed.astype(np.float32)).clone()
+        return normed_inputs_value_normed
+
+    def _load_image_if_cnn(self, idx):
+        image = ""
+
+        if self.args.cnn is None:
+            return image
+
+        filepath = self.df_split.iat[idx, self.index_dict['filepath']]
+        image_path = Path(self.args.image_dir, filepath)
+
+        if self.args.input_channel == 1:
+            image = Image.open(image_path).convert('L')
+        else:
+            image = Image.open(image_path).convert('RGB')
+
+        image = self.augmentation(image)
+        image = self.transform(image)
+        return image
 
     def __len__(self):
         return len(self.df_split)
 
-
-    # Convert normalized values to a single Tensor
-    def _normalized_value_to_single_tensor_if_mlp(self, idx):
-        inputs_value_normed = ""
-
-        if self.args["mlp"] is None:
-            return inputs_value_normed
-
-        index_input_list_normed = [
-            self.index_dict[input_normed] for input_normed in self.input_list_normed
-        ]
-        s_inputs_value_normed = self.df_split.iloc[idx, index_input_list_normed]
-        inputs_value_normed = np.array(s_inputs_value_normed, dtype=np.float64)
-        inputs_value_normed = torch.from_numpy(inputs_value_normed.astype(np.float32)).clone()
-
-        return inputs_value_normed
-
-
-    # Load imgae when CNN or MLP+CNN
-    def _load_image_if_cnn(self, idx):
-        image = ""
-
-        if self.args["cnn"] is None:
-            return image
-
-        filepath = self.df_split.iat[idx, self.index_dict[self.filepath_column]]
-        image_path = os.path.join(self.image_dir, filepath)
-
-        assert (self.args['input_channel'] == 1) or (self.args['input_channel'] == 3), f"Invalid input channel: {self.args['input_channel']}."
-        if self.args['input_channel'] == 1:
-            image = Image.open(image_path).convert('L')   # Specify 8bit grayscale explicitly
-        else:
-            # self.args['input_channel'] == 3
-            image = Image.open(image_path).convert('RGB')
-
-        image = self.augmentation(image)   # augmentation
-        image = self.transform(image)      # transform, ie. To_Tensor() and Normalization
-
-        return image
-
-
-    @abstractmethod
     def __getitem__(self, idx):
-        pass
-
-
-    @classmethod
-    @abstractmethod
-    def create_dataloader(cls, args, csv_dict, images_dir, split_list=None, batch_size=None, sampler=None):
-        pass
-
-
-class MultiLabelDataSet(NervusDataSet):
-    def __init__(self, args, split_provider, image_dir, split_list):
-        multi_label = True
-        super().__init__(args, split_provider, image_dir, split_list, multi_label)
-
-    def __getitem__(self, idx):
-        id = self.df_split.iat[idx, self.index_dict[self.id_column]]
-        institution = self.df_split.iat[idx, self.index_dict[self.institution_column]]
-        examid = self.df_split.iat[idx, self.index_dict[self.examid_column]]
-        raw_label_dict = {row_label_name: self.df_split.iat[idx, self.index_dict[row_label_name]] for row_label_name in self.raw_label_list}
+        institution = self.df_split.iat[idx, self.index_dict['Institution']]
+        examid = self.df_split.iat[idx, self.index_dict['ExamID']]
+        filepath = self.df_split.iat[idx, self.index_dict['filepath']]
+        raw_label_dict = {raw_label_name: self.df_split.iat[idx, self.index_dict[raw_label_name]] for raw_label_name in self.raw_label_list}
         internal_label_dict = {internal_label_name: self.df_split.iat[idx, self.index_dict[internal_label_name]] for internal_label_name in self.internal_label_list}
-        split = self.df_split.iat[idx, self.index_dict[self.split_column]]
-
-        # Convert normalized values to a single Tensor
-        inputs_value_normed = self._normalized_value_to_single_tensor_if_mlp(idx)
-        # Load imgae when CNN or MLP+CNN
+        normed_inputs_value = self._normalized_value_to_single_tensor_if_mlp(idx)
         image = self._load_image_if_cnn(idx)
+        split = self.df_split.iat[idx, self.index_dict['split']]
+        return {
+                'Institution': institution,
+                'ExamID': examid,
+                'Filename': filepath,
+                'raw_labels': raw_label_dict,
+                'internal_labels': internal_label_dict,
+                'normed_inputs': normed_inputs_value,
+                'image': image,
+                'split': split
+                }
 
-        return id, institution, examid, raw_label_dict, internal_label_dict, inputs_value_normed, image, split
+
+def _make_sampler(split_data):
+    _target = []
+    for _, data in enumerate(split_data):
+        # _target.append(list(data['internal_labels'].values())[0])
+        aaa = list(data['internal_labels'].values())[0]
+        print(aaa)
+
+    breakpoint()
+    class_sample_count = np.array([len(np.where(_target == t)[0]) for t in np.unique(_target)])
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in _target])
+    sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
+    return sampler
 
 
-    @classmethod
-    def create_dataloader(cls, args, split_provider, images_dir, split_list=None, batch_size=None, sampler=None):
-        assert (split_list is not None), 'Specify split to make dataloader.'
-        assert (sampler == 'no'), 'samper should be no when multi-ouputs classification or multi-outputs regresson, but yes was specified.'
+def create_dataloader(args, split_provider, split_list=None, batch_size=None, sampler=None):
+    split_data = NervusDataSet(args, split_provider, split_list)
 
-        split_data = cls(args, split_provider, images_dir, split_list)
-        split_loader = DataLoader(
-                                dataset = split_data,
-                                batch_size = batch_size,
-                                shuffle = True,
-                                num_workers = 0,
-                                sampler = None)
-        return split_loader
+    if (args.task == 'classification') or (args.task == 'deepsurv'):
+        if sampler == 'yes':
+            if len(split_provider.raw_label_list) == 1:
+                sampler = _make_sampler(split_data)
+                shuffle = False
+            else:
+                logger.error("Cannot make sampler for multi-label.")
+        else:
+            sampler = None
+            shuffle = True
+    else:
+        if sampler == 'yes':
+            logger.error("Cannot make sampler in regression.")
+        else:
+            sampler = None
+            shuffle = True
+
+    split_loader = DataLoader(
+                            dataset=split_data,
+                            batch_size=batch_size,
+                            shuffle=shuffle,
+                            num_workers=0,
+                            sampler=sampler
+                            )
+    return split_loader
