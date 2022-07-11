2c67137d9c4ea90daa606bbd327a52faa1ce74c0
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-07-11 17:11:49 +0900

    Moved logger.py logger/.

---
+++ logger/logger.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+class NervusLogger:
+    _unexecuted_configure = True
+
+    @classmethod
+    def get_logger(cls, filename):
+        if cls._unexecuted_configure:
+            cls._init_logger()
+
+        return logging.getLogger('nervus.{}'.format(filename))
+
+    @classmethod
+    def set_level(cls, level):
+        _nervus_root_logger = logging.getLogger('nervus')
+        _nervus_root_logger.setLevel(level)
+
+    @classmethod
+    def _init_logger(cls):
+        _nervus_root_logger = logging.getLogger('nervus')
+        _nervus_root_logger.setLevel(logging.INFO)
+
+        # uppper warining
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.WARNING)
+        format = logging.Formatter('%(levelname)-8s %(message)s')
+        ch.setFormatter(format)
+        ch.addFilter(lambda log_record: log_record.levelno >= logging.WARNING)
+        _nervus_root_logger.addHandler(ch)
+
+        # lower warning
+        ch_info = logging.StreamHandler()
+        ch_info.setLevel(logging.DEBUG)
+        ch_info.addFilter(lambda log_record: log_record.levelno < logging.WARNING)
+        _nervus_root_logger.addHandler(ch_info)
+
+        cls._unexecuted_configure = False
