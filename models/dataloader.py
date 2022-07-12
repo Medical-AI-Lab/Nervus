3f432f341548252d674932b40e308e0f659e17a5
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-07-12 10:59:13 +0900

    Deleted code for normalization of tabular data since no need to normalize.

--- models/dataloader.py
+++ models/dataloader.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 from pathlib import Path
 import numpy as np
-import pandas as pd
 
 import torch
 import torchvision.transforms as transforms
@@ -12,61 +10,53 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.sampler import WeightedRandomSampler
 from PIL import Image
-from sklearn.preprocessing import MinMaxScaler
-
-sys.path.append((Path().resolve() / '../').name)
-from logger.logger import NervusLogger
-
-
-logger = NervusLogger.get_logger('dataloder')
 
 
 class NervusDataSet(Dataset):
+    """_summary_
+
+    Args:
+        Dataset (_type_): _description_
+    """
     def __init__(self, args, split_provider, split):
+        """_summary_
+
+        Args:
+            args (_type_): _description_
+            split_provider (_type_): _description_
+            split (_type_): _description_
+        """
         super().__init__()
 
         self.args = args
         self.split_provider = split_provider
         self.split = split
 
+        self.df_source = self.split_provider.df_source
+        self.df_split = self.df_source[self.df_source['split'] == self.split]
+
         self.raw_label_list = self.split_provider.raw_label_list
         self.internal_label_list = self.split_provider.internal_label_list
         self.input_list = self.split_provider.input_list
 
-        self.df_source = self.split_provider.df_source
-        self.df_split = self.df_source[self.df_source['split'] == self.split].copy()  # index is not a serial number.
-        self.df_split.reset_index(inplace=True, drop=True)                            # Without reset_index, nan occurrs in iteration. This may be bacause the index is missing in some place.
+        self.col_index_dict = {col_name: self.df_split.columns.get_loc(col_name) for col_name in self.df_split.columns}
 
-
-        # Nomalize inputs
-        if (self.args.mlp is not None):
-            self.df_train = self.df_source[self.df_source['split'] == 'train'].copy()  # should be normalized with min and max of training data
-            self.scaler = MinMaxScaler()
-            _ = self.scaler.fit(self.df_train[self.input_list])  # only fit
-            self.normed_input_list = ['normed_' + input for input in self.input_list]
-            _normed_inputs = self.scaler.transform(self.df_split[self.input_list])
-            _df_normed_inputs = pd.DataFrame(_normed_inputs, columns=self.normed_input_list)
-            self.df_split = pd.concat([self.df_split, _df_normed_inputs], axis=1)
-
-        # Preprocess for image
         if (self.args.cnn is not None):
             self.transform = self._make_transforms()
             self.augmentation = self._make_augmentations()
 
-        self.index_dict = {col_name: self.df_split.columns.get_loc(col_name) for col_name in self.df_split.columns}  # should be after nomalization inputs
-
     def _make_transforms(self):
+        assert ((self.args.input_channel == 1) or (self.args.input_channel == 3)), f"Invalid input channel: {self.args.input_channel}."
+
         _transforms = []
         _transforms.append(transforms.ToTensor())
 
         if self.args.normalize_image == 'yes':
-            # transforms.Normalize accepts Tensor only.
+            # transforms.Normalize accepts only Tensor.
             if self.args.input_channel == 1:
                 _transforms.append(transforms.Normalize(mean=(0.5, ), std=(0.5, )))
-            elif self.args.input_channel == 3:
-                _transforms.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
             else:
-                logger.error(f"Invalid input channel: {self.args.input_channel}.")
+                _transforms.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
         else:
             pass
 
@@ -74,6 +64,9 @@ class NervusDataSet(Dataset):
         return _transforms
 
     def _make_augmentations(self):
+        _aug_list = ['randaug', 'trivialaugwide', 'augmix', 'noaug']
+        assert (self.args.augmentation in _aug_list), f"Invalid augmentation: {self.args.augmentation}."
+
         _augmentation = []
 
         if self.args.augmentation == 'randaug':
@@ -82,33 +75,31 @@ class NervusDataSet(Dataset):
             _augmentation.append(transforms.TrivialAugmentWide())
         elif self.args.augmentation == 'augmix':
             _augmentation.append(transforms.AugMix())   # ? Cannot find in transforms ?
-        elif (self.args.augmentation == 'no'):
-            pass
         else:
-            logger.error(f"Invalid augmentation: {self.args.augmentation}.")
+            pass
 
         _augmentation = transforms.Compose(_augmentation)
         return _augmentation
 
-    def _normalized_value_to_single_tensor_if_mlp(self, idx):
-        normed_inputs_value_normed = ""
+    def _input_value_to_single_tensor_if_mlp(self, idx):
+        inputs_value = ''
 
         if (self.args.mlp is None):
-            return normed_inputs_value_normed
+            return inputs_value
 
-        index_normed_input_list = [self.index_dict[normed_input] for normed_input in self.normed_input_list]
-        s_normed_inputs_value = self.df_split.iloc[idx, index_normed_input_list]
-        normed_inputs_value_normed = np.array(s_normed_inputs_value, dtype=np.float64)
-        normed_inputs_value_normed = torch.from_numpy(normed_inputs_value_normed.astype(np.float32)).clone()
-        return normed_inputs_value_normed
+        index_input_list = [self.col_index_dict[input] for input in self.input_list]
+        s_inputs_value = self.df_split.iloc[idx, index_input_list]
+        inputs_value = np.array(s_inputs_value, dtype=np.float64)
+        inputs_value = torch.from_numpy(inputs_value.astype(np.float32)).clone()
+        return inputs_value
 
     def _load_image_if_cnn(self, idx):
-        image = ""
+        image = ''
 
         if self.args.cnn is None:
             return image
 
-        filepath = self.df_split.iat[idx, self.index_dict['filepath']]
+        filepath = self.df_split.iat[idx, self.col_index_dict['filepath']]
         image_path = Path(self.args.image_dir, filepath)
 
         if self.args.input_channel == 1:
@@ -124,21 +115,21 @@ class NervusDataSet(Dataset):
         return len(self.df_split)
 
     def __getitem__(self, idx):
-        institution = self.df_split.iat[idx, self.index_dict['Institution']]
-        examid = self.df_split.iat[idx, self.index_dict['ExamID']]
-        filepath = self.df_split.iat[idx, self.index_dict['filepath']]
-        raw_label_dict = {raw_label_name: self.df_split.iat[idx, self.index_dict[raw_label_name]] for raw_label_name in self.raw_label_list}
-        internal_label_dict = {internal_label_name: self.df_split.iat[idx, self.index_dict[internal_label_name]] for internal_label_name in self.internal_label_list}
-        normed_inputs_value = self._normalized_value_to_single_tensor_if_mlp(idx)
+        institution = self.df_split.iat[idx, self.col_index_dict['Institution']]
+        examid = self.df_split.iat[idx, self.col_index_dict['ExamID']]
+        filepath = self.df_split.iat[idx, self.col_index_dict['filepath']]
+        raw_label_dict = {raw_label_name: self.df_split.iat[idx, self.col_index_dict[raw_label_name]] for raw_label_name in self.raw_label_list}
+        internal_label_dict = {internal_label_name: self.df_split.iat[idx, self.col_index_dict[internal_label_name]] for internal_label_name in self.internal_label_list}
+        inputs_value = self._input_value_to_single_tensor_if_mlp(idx)
         image = self._load_image_if_cnn(idx)
-        split = self.df_split.iat[idx, self.index_dict['split']]
+        split = self.df_split.iat[idx, self.col_index_dict['split']]
         return {
                 'Filename': Path(filepath).name,
                 'ExamID': examid,
                 'Institution': institution,
                 'raw_labels': raw_label_dict,
                 'internal_labels': internal_label_dict,
-                'normed_inputs': normed_inputs_value,
+                'inputs': inputs_value,
                 'image': image,
                 'split': split
                 }
