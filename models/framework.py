9a7aff85a6adc0ebacac97220cb6712682cbac83
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-07-15 17:46:39 +0900

    Modified code for MLP to make it available multi-label.

--- models/framework.py
+++ models/framework.py
@@ -1,2 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+
+import torch.nn as nn
+from torchinfo import summary
+
+from net import FeatureExtractorMixin
+
+
+class MultiLabelMixin:
+    def fork_forward(self, x):
+        multi_output = dict()
+        for fc_name, fc in self.multi_fc.items():
+            multi_output[fc_name] = fc(x)
+        return multi_output
+
+    def get_output_of_label(self, internal_label_name):
+        return self.multi_outputs['fc_' + internal_label_name]
+
+
+class MultiLabelWidget(nn.Module, FeatureExtractorMixin, MultiLabelMixin):
+    """
+    Class for a widght to inherit multiple base classes simultaneously
+    """
+    pass
+
+
+class MultiMLPNet(MultiLabelWidget):
+    def __init__(self, num_inputs, num_classes_in_internal_label):
+        super().__init__()
+
+        self.num_inputs = num_inputs
+        self.num_classes_in_internal_label = num_classes_in_internal_label
+
+        _mlp = self.set_net('MLPNet')(self.num_inputs)
+        self.extractor = self.create_extractor(_mlp, 'MLPNet')   # * _mlp should be in nn.Module.
+        self.multi_fc = self._build_multi_fc()
+
+    def _build_multi_fc(self):
+        _multi_fc = nn.ModuleDict()
+        _fc_in_features = self.extractor.mlp[-2].out_features
+        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
+            _multi_fc['fc_' + internal_label_name] = nn.Linear(_fc_in_features, num_classes)
+        return _multi_fc
+
+    def forward(self, x):
+        x = self.extractor(x)['feature']  # * reference key should be specified to extract output from extractor.
+        multi_output = self.fork_forward(x)
+        return multi_output
+
+
+def create_model(args, split_provider, gpu_ids=None):
+    pass
+    # 1ch, 3ch
+    # multi
+    # MLP+CNN
--- models/net.py
+++ models/net.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import torchvision.models as models
+import torch.nn as nn
 from torchvision.ops import MLP
+import torchvision.models as models
+from torchvision.models.feature_extraction import create_feature_extractor
+
+
+# * Once num_classis is supposed to be 1000 as defined in models.resnet18
+class MLPNet(nn.Module):
+    def __init__(self, num_inputs, num_classes=1000):
+        super().__init__()
+        self.num_inputs = num_inputs
+        self.num_classes = num_classes
+
+        # Configure of MLP
+        self.hidden_channels = [256, 256, 256]
+        self.dropout = 0.2
+
+        self.mlp = MLP(in_channels=self.num_inputs, hidden_channels=self.hidden_channels, dropout=self.dropout)
+        self.fc_in_features = self.hidden_channels[-1]
+        self.fc = nn.Linear(self.fc_in_features, self.num_classes)
+
+    def forward(self, x):
+        x = self.mlp(x)
+        output = self.fc(x)
+        return output
 
 
 cnns = {
@@ -26,14 +49,44 @@ vits = {
     'ViTl32': models.vit_l_32
     }
 
-mlp = {
-    'MLP': MLP
+# * {'参照したい層': '参照したい名前'}
+ref_feature = 'feature'
+feature_extractors = {
+    'ResNet18': {'avgpool': ref_feature},
+    'ResNet': {'avgpool': ref_feature},
+    'DenseNet': {'features': ref_feature},
+    'B0': {'avgpool': ref_feature},
+    'B2': {'avgpool': ref_feature},
+    'B4': {'avgpool': ref_feature},
+    'B6': {'avgpool': ref_feature},
+    'ConvNeXtTiny': {'avgpool': ref_feature},
+    'ConvNeXtSmall': {'avgpool': ref_feature},
+    'ConvNeXtBase': {'avgpool': ref_feature},
+    'ConvNeXtLarge': {'avgpool': ref_feature},
+    'ViTb16': {'encoder.ln': ref_feature},
+    'ViTb32': {'encoder.ln': ref_feature},
+    'ViTl16': {'encoder.ln': ref_feature},
+    'ViTl32': {'encoder.ln': ref_feature},
+    'MLPNet': {'mlp': ref_feature}
     }
 
-nets = {**cnns, **vits, **mlp}
+nets = {**cnns, **vits, **{'MLPNet': MLPNet}}
 
 
-def set_net(net_name):
-    assert (net_name in nets), f"No specified net: {net_name}."
+class BaseClassifier:
+    def __init__(self):
+        pass
 
-    return nets[net_name]
+
+class FeatureExtractorMixin:
+    def set_net(self, net_name):
+        assert (net_name in nets), f"No specified net: {net_name}."
+        return nets[net_name]
+
+    def create_extractor(self, net, net_name):
+        # net = self.set_net(net_name)
+        extractor = create_feature_extractor(net, feature_extractors[net_name])
+        return extractor
+
+    def get_feature(self, feature):
+        return feature[ref_feature]
