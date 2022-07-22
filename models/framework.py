c2240ab23f81454604d1f68242b77e3302efd98b
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-07-22 09:47:44 +0900

    Defined MLP, CNN and ViT for multi-label.

--- models/framework.py
+++ models/framework.py
@@ -24,52 +24,17 @@ class MultiWidget(nn.Module, BaseNet, MultiMixin):
     """
     pass
 
-class MultiMLPNet(MultiWidget):
-    def __init__(self, num_inputs, num_classes_in_internal_label):
-        super().__init__()
 
-        self.num_inputs = num_inputs
-        self.num_classes_in_internal_label = num_classes_in_internal_label
-        self.extractor = self.MLPNet(self.num_inputs)
-        self.multi_classifier = self._construct_multi_classifier()
-
-    def _construct_multi_classifier(self):
-        in_features = self.extractor[-2].out_features
-        classifiers = dict()
-        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
-            classifiers[internal_label_name] = nn.Linear(in_features, num_classes)
-
-        multi_classifier = nn.ModuleDict(classifiers)
-        return multi_classifier
-
-    def forward(self, x):
-        out_features = self.extractor(x)
-        output = self.multi_forward(out_features)
-        return output
-
-
-class MultiResNet(MultiWidget):
-    def __init__(self, net_name, num_classes_in_internal_label):
+class MultiNet(MultiWidget):
+    def __init__(self, net_name, num_classes_in_internal_label, mlp_num_inputs=None, vit_image_size=None):
         super().__init__()
 
         self.net_name = net_name
+        self.mlp_num_inputs = mlp_num_inputs
+        self.vit_image_size = vit_image_size
         self.num_classes_in_internal_label = num_classes_in_internal_label
-        self.extractor = self._create_feature_extractor()
-        self.multi_classifier = self._construct_multi_classifier()
-
-    def _create_feature_extractor(self):
-        extractor = self.set_net(self.net_name)
-        extractor.fc = self.DUMMY
-        return extractor
-
-    def _construct_multi_classifier(self):
-        in_features = self.set_net(self.net_name).fc.in_features
-        classifiers = dict()
-        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
-            classifiers[internal_label_name] = nn.Linear(in_features, num_classes)
-
-        multi_classifier = nn.ModuleDict(classifiers)
-        return multi_classifier
+        self.extractor = self.constuct_extractor(self.net_name, mlp_num_inputs=self.mlp_num_inputs, vit_image_siz=self.vit_image_size)
+        self.multi_classifier = self.construct_multi_classifier(self.net_name, self.num_classes_in_internal_label)
 
     def forward(self, x):
         out_features = self.extractor(x)
@@ -77,105 +42,8 @@ class MultiResNet(MultiWidget):
         return output
 
 
-class MultiDenseNet(MultiWidget):
-    def __init__(self, net_name, num_classes_in_internal_label):
-        super().__init__()
-
-        self.net_name = net_name
-        self.num_classes_in_internal_label = num_classes_in_internal_label
-        self.extractor = self._create_feature_extractor()
-        self.multi_classifier = self._construct_multi_classifier()
-
-    def _create_feature_extractor(self):
-        extractor = self.set_net(self.net_name)
-        extractor.classifier = self.DUMMY
-        return extractor
-
-    def _construct_multi_classifier(self):
-        in_features = self.set_net(self.net_name).classifier.in_features
-        classifiers = dict()
-        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
-            classifiers[internal_label_name] = nn.Linear(in_features, num_classes)
-
-        multi_classifier = nn.ModuleDict(classifiers)
-        return multi_classifier
-
-    def forward(self, x):
-        out_features = self.extractor(x)
-        output = self.multi_forward(out_features)
-        return output
-
-
-class MultiEfficientNet(MultiWidget):
-    def __init__(self, net_name, num_classes_in_internal_label):
-        super().__init__()
-
-        self.net_name = net_name
-        self.num_classes_in_internal_label = num_classes_in_internal_label
-        self.extractor = self._create_feature_extractor()
-        self.multi_classifier = self._construct_multi_classifier()
-
-    def _create_feature_extractor(self):
-        extractor = self.set_net(self.net_name)
-        extractor.classifier = self.DUMMY
-        return extractor
-
-    def _construct_multi_classifier(self):
-        base_classifier = self.set_net(self.net_name).classifier
-        dropout = base_classifier[0].p
-        in_features = base_classifier[1].in_features
-        classifiers = dict()
-        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
-            classifiers[internal_label_name] = nn.Sequential(
-                                                    nn.Dropout(p=dropout, inplace=False),
-                                                    nn.Linear(in_features, num_classes)
-                                                )
-        multi_classifier = nn.ModuleDict(classifiers)
-        return multi_classifier
-
-    def forward(self, x):
-        out_features = self.extractor(x)
-        output = self.multi_forward(out_features)
-        return output
-
-
-class MultiConvNeXt(MultiWidget):
-    def __init__(self, net_name, num_classes_in_internal_label):
-        super().__init__()
-
-        self.net_name = net_name
-        self.num_classes_in_internal_label = num_classes_in_internal_label
-        self.extractor = self._create_feature_extractor()
-        self.multi_classifier = self._construct_multi_classifier()
-
-    def _create_feature_extractor(self):
-        extractor = self.set_net(self.net_name)
-        extractor.classifier = self.DUMMY
-        return extractor
-
-    def _construct_multi_classifier(self):
-        base_classifier = self.set_net(self.net_name).classifier
-        layer_norm = base_classifier[0]
-        flatten = base_classifier[1]
-        in_features = base_classifier[2].in_features
-        classifiers = dict()
-        for internal_label_name, num_classes in self.num_classes_in_internal_label.items():
-            classifiers[internal_label_name] = nn.Sequential(
-                                                    layer_norm,
-                                                    flatten,
-                                                    nn.Linear(in_features, num_classes)
-                                                )
-        multi_classifier = nn.ModuleDict(classifiers)
-        return multi_classifier
-
-    def forward(self, x):
-        out_features = self.extractor(x)
-        output = self.multi_forward(out_features)
-        return output
-
-
-# MLP+CNN
-class MultiMLPCNN(MultiWidget):
+# MLP+(CNN or ViT)
+class MultiNetFusion(MultiWidget):
     def __init__(self, num_inputs, net_name, num_classes_in_internal_label):
         super().__init__()
 
@@ -184,14 +52,16 @@ class MultiMLPCNN(MultiWidget):
         self.num_classes_in_internal_label = num_classes_in_internal_label
 
         self.mlp = self.MLPNet(self.num_inputs)
-        self.cnn = self._create_extracrtor(self.net_name)
+        self.cnn_vit = self._create_extracrtor(self.net_name)
         self.num_inputs_all = 1
         self.multi_classifier = self.MLPNet(self.num_inputs)
 
     def _create_feature_extratror(self):
+        # To be defined
         pass
 
     def forward(self, inputs, x):
+        # To be defined
         pass
 
 
--- models/net.py
+++ models/net.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-r
 
+from collections import OrderedDict
 import torch.nn as nn
 import torchvision.models as models
 from torchvision.ops import MLP
 
 
 class BaseNet:
-    nets = {
+    cnn = {
             'ResNet18': models.resnet18,
             'ResNet': models.resnet50,
             'DenseNet': models.densenet161,
@@ -18,79 +19,147 @@ class BaseNet:
             'ConvNeXtTiny': models.convnext_tiny,
             'ConvNeXtSmall': models.convnext_small,
             'ConvNeXtBase': models.convnext_base,
-            'ConvNeXtLarge': models.convnext_large,
+            'ConvNeXtLarge': models.convnext_large
+        }
+
+    vit = {
             'ViTb16': models.vit_b_16,
             'ViTb32': models.vit_b_32,
             'ViTl16': models.vit_l_16,
             'ViTl32': models.vit_l_32
-            }
+        }
 
-    DUMMY = nn.Identity()
+    vit_weight = {
+                    'ViTb16': models.ViT_B_16_Weights.DEFAULT,
+                    'ViTb32': models.ViT_B_32_Weights.DEFAULT,
+                    'ViTl16': models.ViT_L_16_Weights.DEFAULT,
+                    'ViTl32': models.ViT_L_32_Weights.DEFAULT
+                }
 
-    @classmethod
-    def set_net(cls, net_name):
-        assert (net_name in cls.nets), f"No specified net: {net_name}."
-        net = cls.nets[net_name]()
-        return net
+    net = {**cnn, **vit}
+
+    classifier = {
+                'ResNet18': 'fc',
+                'ResNet': 'fc',
+                'DenseNet': 'classifier',
+                'B0': 'classifier',
+                'B2': 'classifier',
+                'B4': 'classifier',
+                'B6': 'classifier',
+                'ConvNeXtTiny': 'classifier',
+                'ConvNeXtSmall': 'classifier',
+                'ConvNeXtBase': 'classifier',
+                'ConvNeXtLarge': 'classifier',
+                'ViTb16': 'heads',
+                'ViTb32': 'heads',
+                'ViTl16': 'heads',
+                'ViTl32': 'heads'
+                }
+
+    mlp_config = {
+                'hidden_channels': [256, 256, 256],
+                'dropout': 0.2
+                }
 
     @classmethod
     def MLPNet(cls, num_inputs):
-        """
-        Args:
-            num_inputs (int): the number of inputs
-
-        Returns:
-            torchvision.ops.misc.MLP : MLP has no output layer.
-        """
-        hidden_channels = [256, 256, 256]
-        dropout = 0.2
-        mlp = MLP(in_channels=num_inputs, hidden_channels=hidden_channels, dropout=dropout)
+        mlp = MLP(in_channels=num_inputs, hidden_channels=cls.mlp_config['hidden_channels'], dropout=cls.mlp_config['dropout'])
         return mlp
 
     @classmethod
-    def create_feature_extractor(cls, net_name):
-        extractor = cls.nets[net_name]()
-        if net_name.startswith('ResNet'):
-            extractor.fc = cls.DUMMY
-        elif net_name.startswith('DenseNet'):
-            extractor.classifier = cls.DUMMY
-        elif net_name.startswith('B'):
-            extractor.classifier = cls.DUMMY
-        elif net_name.startswith('Conv'):
-            extractor.classifier = cls.DUMMY
+    def set_net(cls, net_name, vit_image_size=None):
+        if net_name in cls.cnn:
+            net = cls.cnn[net_name]()
         else:
-            # ViT
-            extractor.heads = cls.DUMMY
+            assert isinstance(vit_image_size, int), f"Invalid image size for ViT: {vit_image_size}."
+            net = cls.set_vit(cls, net_name, vit_image_size)
+        return net
+
+    @classmethod
+    def set_vit(cls, net_name, vit_image_size):
+        base_vit = cls.vit[net_name]
+        pretrained_vit = base_vit(pretrained=cls.vit_weight[net_name])
+
+        if vit_image_size == 224:
+            return pretrained_vit  # Default
+        else:
+            # Modify shape of weight
+            weight = pretrained_vit.state_dict()
+            patch_size = int(net_name[-2:])  # 'ViTb16' -> 16
+            aligned_weight = models.vision_transformer.interpolate_embeddings(
+                                                        image_size=vit_image_size,
+                                                        patch_size=patch_size,
+                                                        model_state=weight
+                                                        )
+            aligned_vit = base_vit(image_size=vit_image_size)
+            aligned_vit.load_state_dict(aligned_weight)
+            return aligned_vit
+
+    @classmethod
+    def constuct_extractor(cls, net_name, mlp_num_inputs=None, vit_image_size=None):
+        if net_name == 'MLP':
+            assert isinstance(mlp_num_inputs, int), f"Invalid number of inputs for MLP: {mlp_num_inputs}."
+            extractor = cls.MLPNet(mlp_num_inputs)
+        else:
+            assert net_name in cls.net, f"No specified net: {net_name}."
+            DUMMY_CLASSIFIER = nn.Identity()
+            extractor = cls.set_net(net_name, vit_image_size)
+            extractor._modules[cls.classifier[net_name]] = DUMMY_CLASSIFIER
         return extractor
 
     @classmethod
     def get_classifier(cls, net_name):
-        net = cls.nets[net_name]()
-        if net_name.startswith('ResNet'):
-            classifier = net.fc
-        elif net_name.startswith('DenseNet'):
-            classifier = net.classifier
-        elif net_name.startswith('B'):
-            classifier = net.classifier
-        elif net_name.startswith('Conv'):
-            classifier = net.classifier
-        else:
-            # ViT
-            classifier = net.heads
+        net = cls.net[net_name]()
+        classifier = net._modules[cls.classifier[net_name]]
         return classifier
 
     @classmethod
-    def get_in_features(cls, net_name):
-        net = cls.nets[net_name]()
-        if net_name.startswith('ResNet'):
-            in_features = net.fc.in_features
-        elif net_name.startswith('DenseNet'):
-            in_features = net.classifier.in_features
+    def construct_multi_classifier(cls, net_name, num_classes_in_internal_label):
+        classifiers = dict()
+
+        if net_name == 'MLP':
+            in_features = cls.mlp_config['hidden_channels'][-1]
+            for internal_label_name, num_classes in num_classes_in_internal_label.items():
+                classifiers[internal_label_name] = nn.Linear(in_features, num_classes)
+
+        elif net_name.startswith('ResNet') or net_name.startswith('DenseNet'):
+            base_classifier = cls.get_classifier(net_name)
+            in_features = base_classifier.in_features
+            for internal_label_name, num_classes in num_classes_in_internal_label.items():
+                classifiers[internal_label_name] = nn.Linear(in_features, num_classes)
+
         elif net_name.startswith('B'):
-            in_features = net.classifier[1].in_features
-        elif net_name.startswith('Conv'):
-            in_features = net.classifier[2].in_features
+            base_classifier = cls.get_classifier(net_name)
+            dropout = base_classifier[0].p
+            in_features = base_classifier[1].in_features
+            for internal_label_name, num_classes in num_classes_in_internal_label.items():
+                classifiers[internal_label_name] = nn.Sequential(
+                                                    nn.Dropout(p=dropout, inplace=False),
+                                                    nn.Linear(in_features, num_classes)
+                                                    )
+
+        elif net_name.startswith('ConvNeXt'):
+            base_classifier = cls.get_classifier(net_name)
+            layer_norm = base_classifier[0]
+            flatten = base_classifier[1]
+            in_features = base_classifier[2].in_features
+            for internal_label_name, num_classes in num_classes_in_internal_label.items():
+                classifiers[internal_label_name] = nn.Sequential(
+                                                    layer_norm,
+                                                    flatten,
+                                                    nn.Linear(in_features, num_classes)
+                                                    )
         else:
             # ViT
-            in_features = net.heads.head.in_features
-        return in_features
+            # x = self.encoder(x)
+            # Classifier "token" as used by standard language architectures
+            # x = x[:, 0]
+            base_classifier = cls.get_classifier(net_name)
+            in_features = base_classifier.head.in_features
+            for internal_label_name, num_classes in num_classes_in_internal_label.items():
+                classifiers[internal_label_name] = nn.Sequential(OrderedDict([
+                                                        ('head', nn.Linear(in_features, num_classes))
+                                                    ]))
+
+        multi_classifier = nn.ModuleDict(classifiers)
+        return multi_classifier
