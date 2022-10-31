9710dff75e1d52301b7f350ea72d769c1dafc71e
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2022-10-31 16:01:55 +0900

    Updated.

--- work_all.sh
+++ work_all.sh
@@ -2,9 +2,8 @@
 
 set -eu
 
-parameter_csv="./parameter.csv"
+#parameter_csv="./parameter.csv"
 
-#python="python3"
 python="python"
 
 train_code="train.py"
@@ -19,49 +18,47 @@ while getopts ":g" optKey; do
   esac
 done
 
-#1  task,
-#2  csv_name,
-#3  image_dir,
-#4  model,
-#5  criterion,
-#6  optimizer,
-#7  epochs,
-#8  batch_size,
-#9  sampler,
-#10 augmtntation,
-#11 in_channel,
-#12 save_weight,
-#13 gpu_ids
+#1  task
+#2  csvpath
+#3  model
+#4  criterion
+#5  optimizer
+#6  epochs
+#7  batch_size
+#8  sampler
+#9  augmtntation
+#10 in_channel
+#11 save_weight_policy
+#12 gpu_ids
 total=$(tail -n +1 "$parameter_csv" | wc -l)
 i=1
 for row in $(tail -n +2 "$parameter_csv"); do
   task=$(echo "$row" | cut -d "," -f1)
-  csv_name=$(echo "$row" | cut -d "," -f2)
-  image_dir=$(echo "$row" | cut -d "," -f3)
-  model=$(echo "$row" | cut -d "," -f4)
-  criterion=$(echo "$row" | cut -d "," -f5)
-  optimizer=$(echo "$row" | cut -d "," -f6)
-  epochs=$(echo "$row" | cut -d "," -f7)
-  batch_size=$(echo "$row" | cut -d "," -f8)
-  sampler=$(echo "$row" | cut -d "," -f9)
-  augmentation=$(echo "$row" | cut -d "," -f10)
-  in_channel=$(echo "$row" | cut -d "," -f11)
-  save_weight=$(echo "$row" | cut -d "," -f12)
-  gpu_ids=$(echo "$row" | cut -d "," -f13)
+  csvpath=$(echo "$row" | cut -d "," -f2)
+  model=$(echo "$row" | cut -d "," -f3)
+  criterion=$(echo "$row" | cut -d "," -f4)
+  optimizer=$(echo "$row" | cut -d "," -f5)
+  epochs=$(echo "$row" | cut -d "," -f6)
+  batch_size=$(echo "$row" | cut -d "," -f7)
+  sampler=$(echo "$row" | cut -d "," -f8)
+  augmentation=$(echo "$row" | cut -d "," -f9)
+  in_channel=$(echo "$row" | cut -d "," -f10)
+  save_weight_policy=$(echo "$row" | cut -d "," -f11)
+  gpu_ids=$(echo "$row" | cut -d "," -f12)
 
   echo "$i/$total: Training starts..."
   echo ""
 
   # Traning
-  echo "$python $train_code --task $task --csv_name $csv_name --image_dir $image_dir --model $model --criterion $criterion --optimizer $optimizer --epochs $epochs --batch_size $batch_size --sampler $sampler --augmentation $augmentation --in_channel $in_channel --save_weight $save_weight --gpu_ids $gpu_ids"
-  "$python" "$train_code" --task "$task" --csv_name "$csv_name" --image_dir "$image_dir" --model "$model" --criterion "$criterion" --optimizer "$optimizer" --epochs "$epochs" --batch_size "$batch_size" --sampler "$sampler" --augmentation "$augmentation" --in_channel "$in_channel" --save_weight "$save_weight" --gpu_ids "$gpu_ids"
+  echo "$python $train_code --task $task --csvpath $csvpath  --model $model --criterion $criterion --optimizer $optimizer --epochs $epochs --batch_size $batch_size --sampler $sampler --augmentation $augmentation --in_channel $in_channel --save_weight_policy $save_weight_policy --gpu_ids $gpu_ids"
+  "$python" "$train_code" --task "$task" --csvpath "$csvpath" --model "$model" --criterion "$criterion" --optimizer "$optimizer" --epochs "$epochs" --batch_size "$batch_size" --sampler "$sampler" --augmentation "$augmentation" --in_channel "$in_channel" --save_weight_policy "$save_weight_policy" --gpu_ids "$gpu_ids"
 
   echo ""
 
-  # Test
+  # Internal Test
   echo "$i/$total: Test starts..."
-  echo "$python $test_code"
-  "$python" "$test_code"
+  echo "$python $test_code --csvpath $csvpath"
+  "$python" "$test_code" --csvpath "$csvpath"
 
   echo ""
 
@@ -69,6 +66,8 @@ for row in $(tail -n +2 "$parameter_csv"); do
   echo "$i/$total: Evaluation starts..."
   echo "$python $eval_code"
   "$python" "$eval_code"
+
+echo ""
 
   i=$(($i + 1))
   echo -e ""
