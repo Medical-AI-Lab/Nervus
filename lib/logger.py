a34f2fc1492daf7a2b32c361fb4521c6d23c7713
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2023-01-17 15:11:35 +0900

    Modified formatter of both stream handler and file handler when error occurs.

--- lib/logger.py
+++ lib/logger.py
@@ -33,18 +33,38 @@ class BaseLogger:
         """
         _root_logger = logging.getLogger('nervus')
         _root_logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        # file handler
         log_dir = Path('./logs')
         log_dir.mkdir(parents=True, exist_ok=True)
         log_path = Path(log_dir, 'log.log')
+
+        # file handler
+        ## upper warning
+        fh_err = logging.FileHandler(log_path)
+        fh_err.setLevel(logging.WARNING)
+        fh_err.setFormatter(formatter)
+        fh_err.addFilter(lambda log_record: log_record.levelno >= logging.WARNING)
+        _root_logger.addHandler(fh_err)
+
+        ## lower warning
         fh = logging.FileHandler(log_path)
         fh.setLevel(logging.DEBUG)
+        fh.addFilter(lambda log_record: log_record.levelno < logging.WARNING)
         _root_logger.addHandler(fh)
 
         # stream handler
+        ## upper warning
+        ch_err = logging.StreamHandler()
+        ch_err.setLevel(logging.WARNING)
+        ch_err.setFormatter(formatter)
+        ch_err.addFilter(lambda log_record: log_record.levelno >= logging.WARNING)
+        _root_logger.addHandler(ch_err)
+
+        ## lower warning
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
+        ch.addFilter(lambda log_record: log_record.levelno < logging.WARNING)
         _root_logger.addHandler(ch)
 
         cls._unexecuted_configure = False
