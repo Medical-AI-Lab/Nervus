fed0fc61ec09ced3053426b17407f93dc3b40627
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2023-01-19 18:22:27 +0900

    Changed from --datetime_dir to --likelihood_dir.

--- eval.py
+++ eval.py
@@ -3,7 +3,6 @@
 
 import argparse
 from pathlib import Path
-import glob
 import re
 import json
 from lib import set_eval, BaseLogger
@@ -22,8 +21,9 @@ class EvalOptions:
         Options for evaluation.
         """
         self.parser = argparse.ArgumentParser(description='Options for evaluation')
-        self.parser.add_argument('--datetime_dir', type=str, default=None, help='Directory of datetime contains target likekihoods (Default: None)')
+        self.parser.add_argument('--likelihood_dir', type=str, default=None, help='Directory of likekihoods (Default: None)')
         self.args = self.parser.parse_args()
+
 
     def _get_latest_likelihood_dir(self) -> str:
         """
@@ -31,12 +31,8 @@ class EvalOptions:
 
         Returns:
             str: Directory of likelihood
-
-        Note that:
-        If directory of materials is link, Path('.').glob('**/likelihoods') cannot follow below materials.
-        Therefore, use glob.glob.
         """
-        _likelihood_dirs = glob.glob('**/results/*/sets/*/likelihoods', recursive=True)
+        _likelihood_dirs = list(Path('results').glob('*/trials/*/likelihoods'))
         assert (_likelihood_dirs != []), 'No directory of likelihood.'
         _likelihood_dir = max(_likelihood_dirs, key=lambda likelihood_dir: Path(likelihood_dir).stat().st_mtime)
         return str(_likelihood_dir)
@@ -45,25 +41,45 @@ class EvalOptions:
         """
         Parse options.
         """
-        if self.args.datetime_dir is None:
+        if self.args.likelihood_dir is None:
             _likelihood_dir = self._get_latest_likelihood_dir()
-            _datetime_dir = str(Path(_likelihood_dir).parents[0])
-            setattr(self.args, 'datetime_dir',  _datetime_dir)
+            setattr(self.args, 'likelihood_dir', _likelihood_dir)
+
+        _datetime = Path(_likelihood_dir).parents[0].name
+        setattr(self.args, 'datetime',  _datetime)
 
 
-def check_task(likelihood_dir: str) -> str:
+
+def collect_likelihood(likelihood_dir: str) -> List[Path]:
+    """
+    Return list of likelihood paths.
+
+    Args:
+        datetime_dir (str): directory of datetime including likelihood
+
+    Returns:
+        List[Path]: list of likelihood paths
+    """
+    likelihood_paths = list(Path(likelihood_dir).glob('*.csv'))
+    assert likelihood_paths != [], f"No likelihood in {likelihood_dir}."
+    likelihood_paths.sort(key=lambda path: path.stat().st_mtime)
+    return likelihood_paths
+
+
+def check_task(datetime_name: str) -> str:
     """
     Return task.
 
     Args:
-        likelihood_dir (str): Directory of likelihood
+        datetime_dir (str): directory of datetime at traning
 
     Returns:
         str: task
     """
-    _dataset_dir = re.findall('(.*)/results', likelihood_dir)[0]
-    _datetime_dir = Path(likelihood_dir).name
-    _parameter_path = list(Path(_dataset_dir).glob('results/*/sets/' + _datetime_dir + '/parameters.json'))[0]
+    _parameter_path = list(Path('results').glob('*/trials/' + datetime_name + '/parameters.json'))[0]
+    # If you specify the likelihood of an external dataset,
+    # the project name is different from the csv name in training,
+    # but the _datetime_dir is the same as the datatime in training, which is always one.
     with open(_parameter_path) as f:
         _parameters = json.load(f)
     task = _parameters['task']
@@ -82,29 +98,13 @@ def check_eval_options() -> EvalOptions:
     return opt
 
 
-def collect_likelihood(datetime_dir: str) -> List[Path]:
-    """
-    Return list of likelihood paths.
-
-    Args:
-        datetime_dir (str): path to directory of likelihoods
-
-    Returns:
-        List[Path]: list of likelihood paths
-    """
-    likelihood_paths = list(Path(datetime_dir, 'likelihoods').glob('*.csv'))
-    assert likelihood_paths != [], f"No likelihood in {datetime_dir}."
-    likelihood_paths.sort(key=lambda path: path.stat().st_mtime)
-    return likelihood_paths
-
-
 def main(opt):
     args = opt.args
-    likelihood_paths = collect_likelihood(args.datetime_dir)
-    task = check_task(args.datetime_dir)
+    likelihood_paths = collect_likelihood(args.likelihood_dir)
+    task = check_task(args.datetime)
     task_eval = set_eval(task)
 
-    logger.info(f"Calculating metrics of {task} for {args.datetime_dir}.\n")
+    logger.info(f"Calculating metrics of {task} for {args.likelihood_dir}.\n")
 
     for likelihood_path in likelihood_paths:
         logger.info(likelihood_path.name)
