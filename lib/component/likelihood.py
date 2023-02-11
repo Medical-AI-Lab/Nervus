4bce1ae01d58fcae4531e0199a1af4ac00d6b345
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2023-02-11 23:35:14 +0900

    Modified.

--- lib/component/likelihood.py
+++ lib/component/likelihood.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
+#from pathlib import Path
 import pandas as pd
 import torch
 from typing import List, Dict
@@ -11,8 +11,8 @@ class Likelihood:
     def __init__(self, task: str, num_outputs_for_label: Dict[str, int]) -> None:
         self.task = task
         self.num_outputs_for_label = num_outputs_for_label
-        self.pred_column_list = self._make_pred_columns(self.num_outputs_for_label)
         self.base_column_list = self._set_base_colums(self.task)
+        self.pred_column_list = self._make_pred_columns(self.num_outputs_for_label)
 
     def _set_base_colums(self, task):
         if task == 'classification':
@@ -55,7 +55,7 @@ class Likelihood:
             pred_columns[label_name] = _pred_list
         return pred_columns
 
-    def make_likelihood(self, data: Dict, output: Dict[str, torch.Tensor]) -> pd.DataFrame:
+    def make_form(self, data: Dict, output: Dict[str, torch.Tensor]) -> pd.DataFrame:
             """
             Make DataFrame of likelihood every batch
 
@@ -64,53 +64,21 @@ class Likelihood:
                 output (Dict[str, torch.Tensor]): output of model
             """
 
-            _base = {data[column_name] for column_name in self.base_column_list}
+            _base = {column_name: data[column_name] for column_name in self.base_column_list}
             _df_base = pd.DataFrame(_base)
 
             if any(data['labels']):
                 for label_name, pred in output.items():
                     _df_label = pd.DataFrame({ label_name: [data['labels'][label_name]] })  # Handle one label at a time
+                    pred = pred.to('cpu').detach().numpy().copy()
                     _pred_columns = self.pred_column_list[label_name]
-                    _pred = pred.to('cpu').detach().numpy().copy()
-                    _df_pred = pd.DataFrame(_pred, columns=_pred_columns)
+                    _df_pred = pd.DataFrame(pred, columns=_pred_columns)
                     df_likelihood = pd.concat([_df_base, _df_label, _df_pred], axis=1)
                 return df_likelihood
             else:
                 for label_name, pred in output.items():
+                    pred = pred.to('cpu').detach().numpy().copy()
                     _pred_columns = self.pred_column_list[label_name]
-                    _pred = pred.to('cpu').detach().numpy().copy()
-                    _df_pred = pd.DataFrame(_pred, columns=_pred_columns)
+                    _df_pred = pd.DataFrame(pred, columns=_pred_columns)
                     df_likelihood = pd.concat([_df_base, _df_pred], axis=1)
                 return df_likelihood
-
-
-
-def set_likelihood(task: str, num_outputs_for_label: Dict[str, int]) -> Likelihood:
-    """
-    Set Likelihood
-
-    Args:
-        task (str): task
-        num_outputs_for_label (Dict[str, int]): number of classes for each label
-
-    Returns:
-        Likelihood: Likelihood
-    """
-    return Likelihood(task, num_outputs_for_label)
-
-
-#def writeout_likelihood(df_likelihood: pd.DataFrame, save_path: str, times_written: int) -> None:
-#        """
-#        Write out likelihoood
-#
-#        Args:
-#            save_path (str): sava path of likelihood
-#            count (int): number of times written out
-#
-#        """
-#        if times_written > 0:
-#            df_likelihood.to_csv(save_path, mode='a', header=False)
-#        else:
-#            save_dir = Path(save_path).parents[0]
-#            save_dir.mkdir(parents=True, exist_ok=True)
-#            df_likelihood.to_csv(save_path, index=False)
