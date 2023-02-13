d49d8f6564d74f38b1fd8792ce25a0840a4c1a9e
Toshimasa-Matsumoto <matsumoto.toshimasa@gmail.com>
2023-02-13 15:11:00 +0900

    Modified.

--- test.py
+++ test.py
@@ -12,50 +12,47 @@ from lib.component import (
         create_dataloader,
         set_likelihood
         )
+from pathlib import Path
 
 
 logger = BaseLogger.get_logger(__name__)
 
 
 def main(args):
-    model_params = args['model']
-    datalaoder_params = args['dataloader']
-    conf_params = args['conf']
-    print_params = args['print']
-    print_paramater(print_params)
+    print_paramater(args.print_params)
 
-    test_splits = conf_params.test_splits
-    task = conf_params.task
-    num_outputs_for_label = conf_params.num_outputs_for_label
-    save_datetime_dir = conf_params.save_datetime_dir
-    weight_paths = conf_params.weight_paths
+    model = create_model(args.model_params)
+    test_splits = args.conf_params.test_splits
+    dataloaders = {split: create_dataloader(args.dataloader_params, split=split) for split in test_splits}
 
-    model = create_model(model_params)
-    dataloaders = {split: create_dataloader(datalaoder_params, split=split) for split in test_splits}
-    likelihood = set_likelihood(task, num_outputs_for_label, save_datetime_dir)
+    task = args.conf_params.task
+    num_outputs_for_label = args.conf_params.num_outputs_for_label
+    save_datetime_dir = args.conf_params.save_datetime_dir
+    weight_paths = args.conf_params.weight_paths
+    likelihood = set_likelihood(task, num_outputs_for_label)
 
     for weight_path in weight_paths:
         logger.info(f"Inference ...")
 
-        # weight is orverwritten every time weight is loaded.
-        model.load_weight(weight_path)
+        model.load_weight(weight_path)  # weight is orverwritten every time weight is loaded.
         model.eval()
 
-        likelihood.init_likelihood()
-        for split in test_splits:
-            split_dataloader = dataloaders[split]
-            for i, data in enumerate(split_dataloader):
+        for i, split in enumerate(test_splits):
+            for j, data in enumerate(dataloaders[split]):
                 in_data, _ = model.set_data(data)
 
                 with torch.no_grad():
                     output = model(in_data)
-                    likelihood.make_likelihood(data, output)
+                    df_likelihood = likelihood.make_format(data, output)
 
-        likelihood.save_likelihood(save_datetime_dir, weight_path.split('/')[-1].replace('.pt', '.csv'))
-
-        if len(weight_paths) > 1:
-            model.init_network(model_params)
-
+                if i + j == 0:
+                    save_dir = Path(save_datetime_dir, 'likelihoods')
+                    save_dir.mkdir(parents=True, exist_ok=True)
+                    save_name = 'likelihood_' + Path(weight_path).stem + '.csv'
+                    save_path = Path(save_dir, save_name)
+                    df_likelihood.to_csv(save_path, index=False)
+                else:
+                    df_likelihood.to_csv(save_path, mode='a', index=False, header=False)
 
 if __name__ == '__main__':
     try:
